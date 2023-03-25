47a77971cdde7c34d018c25e86a5a3ddb47c241b
Vijay Omergekar <omergekar.vijay@gmail.com>
2023-03-25 18:29:07 +0530

    Test maven project

---
+++ src/test/java/com/picsanddabs/demo/it/AppITTest.java
@@ -0,0 +1,16 @@
+package com.picsanddabs.demo.it;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.picsanddabs.demo.App;
+
+public class AppITTest {
+
+	@Test
+	public void test() {
+		new App().getDetails();
+	}
+
+}
---
+++ src/test/java/com/picsanddabs/demo/unit/AppTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.picsanddabs.demo.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.picsanddabs.demo.App;
+
+/**
+ * @author Vijay Omergekar
+ *
+ */
+public class AppTest {
+
+	@Test
+	public void test() {
+		new App().getDetails();
+	}
+
+}
